c3d3466eba1a2782965c0abd14c988a0429da319
Alex Howe <mail@ahowe.dev>
2020-11-23 22:36:04 +0000

    Dev

--- DbRepo/ContextExtensions.cs
+++ DbRepo/ContextExtensions.cs
@@ -3,47 +3,31 @@ using System;
 using System.Collections.Generic;
 using System.Reflection;
 using System.Text;
+using System.Linq;
 
 namespace DbRepo
 {
 	public static class ContextExtensions
 	{
-		private static PropertyInfo? GetPropFromType<T>(object search) where T : class
-		{ 
-			foreach (PropertyInfo info in search.GetType().GetProperties())
-			{
-				if (info.PropertyType == typeof(T))
-				{
-					return info;
-				}
-			}
-			return null;
-		}
-		public static DbRepo.DbRepo<T> GetRepo<T>(this DbContext db) where T : class
-		{
-			string name = typeof(T).Name;
-		
-			PropertyInfo? property = db.GetType().GetProperty(name);
-			if (property == null)
-			{
-				//Attempt if pluralised exists
-				property = db.GetType().GetProperty(name + "s");
-				//Loop as a last resort, to avoid performance
-				property = GetPropFromType<DbSet<T>>(db);
-				if (property == null)
-					throw new Exception($"A DbSet for {name} does not exist in the DbContext");
-			}
-			DbSet<T>? set = property.GetValue(db) as DbSet<T>;
-			if (set == null)
-			{
-				property = GetPropFromType<DbSet<T>>(db);
-				if(property == null)
-					throw new Exception($"A DbSet for {name} does not exist in the DbContext");
+		private static PropertyInfo? GetPropFromType<T>(object search) where T : class => search.GetType().GetProperties().First(p => p.PropertyType == typeof(T));
 
-				set = property.GetValue(db) as DbSet<T>;
-				if (set == null)
-					throw new Exception($"{name} is not a valid DbSet");
-			}
+		public static DbRepo<T> GetRepo<T>(this DbContext db) where T : class
+		{
+			string name = nameof(T);
+
+			PropertyInfo? property = db
+				.GetType().GetProperties()
+				//Attempt if pluralised exists
+				.First(p => (p.Name == name || p.Name == name + "s") && p.PropertyType == typeof(T))
+				//Loop as a last resort, to avoid performance
+				?? GetPropFromType<DbSet<T>>(db);
+			
+			if (property == null)
+				throw new Exception($"A DbSet for {name} was not found in the DbContext");
+
+			if(!(property.GetValue(db) is DbSet<T> set))
+				throw new Exception($"{name} is not a valid DbSet");
+
 			return new DbRepo<T>(set, db);
 		}
 	}
